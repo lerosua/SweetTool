463ea0548cce42874d01a5a500cad72032dc0012
lerosua <lerosua@gmail.com>
2023-09-21 23:58:03 +0800

    fix

--- Package.swift
+++ Package.swift
@@ -2,14 +2,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "SweetTool",
+    name: "sweetTool",
     platforms: [.iOS(.v14), .macOS(.v11)],
     products: [
         .library(name: "sweetTool", targets: ["sweetTool"]),
     ],
     dependencies: [
         // 添加依赖项
-		  .package(url: "https://github.com/apple/swift-corelibs-ios.git", from: "1.0.0"),
     ],
     targets: [
         .target(name: "sweetTool", dependencies: [],
