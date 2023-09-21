18fa5dff14a477db93abcafd89f62d5b3934b91f
lerosua <lerosua@gmail.com>
2023-09-21 23:44:58 +0800

    fix

--- Package.swift
+++ Package.swift
@@ -1,17 +1,18 @@
+// swift-tools-version: 5.9
 import PackageDescription
 
-// swift-tools-version: 5.9
 let package = Package(
     name: "SweetTool",
     platforms: [.iOS(.v14), .macOS(.v11)],
     products: [
-        .library(name: "SweetTool", targets: ["SweetTool"]),
+        .library(name: "sweetTool", targets: ["sweetTool"]),
     ],
     dependencies: [
         // 添加依赖项
+		  .package(url: "https://github.com/apple/swift-corelibs-ios.git", from: "1.0.0"),
     ],
     targets: [
-        .target(name: "SweetTool", dependencies: []),
-    ],
-	 toolsVersion: "5.9"
+        .target(name: "sweetTool", dependencies: [],
+		path:"Extension"),
+    ]
 )
