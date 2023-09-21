e292f36e527c9485b4a38a16c23d1ef9b3d601c1
lerosua <lerosua@gmail.com>
2023-09-22 00:04:46 +0800

    调整文件

--- Sources/SweetTool/UIView+ST.swift
+++ Sources/SweetTool/UIView+ST.swift
@@ -1,9 +1,9 @@
 //
-//  UIKit+EX.swift
-//  winwin
+//  UIView+ST.swift
+//  sweetTool
 //
-//  Created by lerosua on 5/21/19.
-//  Copyright © 2019 lerosua. All rights reserved.
+//  Created by rosua le on 2023/9/22.
+//  Copyright © 2023 lerosua. All rights reserved.
 //
 
 import UIKit
@@ -35,21 +35,3 @@ extension UIView {
     }
 }
 
-
-extension UIColor {
-   public convenience init(red: Int, green: Int, blue: Int) {
-        assert(red >= 0 && red <= 255, "Invalid red component")
-        assert(green >= 0 && green <= 255, "Invalid green component")
-        assert(blue >= 0 && blue <= 255, "Invalid blue component")
-        
-        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
-    }
-    
-  public  convenience init(rgb: Int) {
-        self.init(
-            red: (rgb >> 16) & 0xFF,
-            green: (rgb >> 8) & 0xFF,
-            blue: rgb & 0xFF
-        )
-    }
-}
